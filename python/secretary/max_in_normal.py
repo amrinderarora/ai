fb21ca655f08c8f9d3d6af8a15cb0aa4dd85c212
Amrinder Arora <email@domain.tld>
2020-12-10 05:49:25 -0500

    Just some formatting.

--- python/secretary/max_in_normal.py
+++ python/secretary/max_in_normal.py
@@ -1,11 +1,14 @@
 import numpy as np
 import math
 
-mu = 0
-sigma = 1
+mu = 50
+sigma = 5
+print("mu: ", mu, ", sigma: ", sigma)
 for x in range(10,17):
     ex = math.exp(x)
     sizeOfDist = math.floor(ex)
     s = np.random.normal(mu, sigma, sizeOfDist)
     max_s = max(s)
-    print(sizeOfDist, round(math.log(sizeOfDist),4), round(max_s,4), round(x/math.exp(1),4))
+    print("n: ", sizeOfDist, ", log(n): ", round(math.log(sizeOfDist),4), 
+          ", max:", round(max_s,4), ", log(n)/e: ", round(x/math.exp(1),4),
+          ", mu + sigma log(n)/e: ", round(mu + sigma * x/math.exp(1),4))
