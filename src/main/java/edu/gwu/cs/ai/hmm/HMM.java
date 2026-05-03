d3ecad1c790648126f5d4a440eabfa329275cafe
amrinderarora <amrinderarora@172.16.1.66>
2026-05-02 22:14:01 -0400

    HMM and NNs code..

--- src/main/java/edu/gwu/cs/ai/hmm/HMM.java
+++ src/main/java/edu/gwu/cs/ai/hmm/HMM.java
@@ -1,20 +1,66 @@
 package edu.gwu.cs.ai.hmm;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Models a hidden Markov Model. 
+ * 
+ * @author Amrinder Arora
+ */
 public class HMM {
-    private int numStates; // Same as numEmissions
+	// States
+    private String[] states;
+    private Map<String, Integer> stateIndexMap = new HashMap<>();
+    private String[] emissions;
+    private Map<String, Integer> emissionIndexMap = new HashMap<>();
     private double[][] transitionProb;
     private double[][] emissionProb;
-
-    public int getNumStates() {
-        return numStates;
+    
+    /** Sets the states using the given states array. */
+    public void setStates(String[] statesArg) {
+    	this.states = statesArg;
+    	int idx = 0;
+    	for (String s: statesArg) {
+    		stateIndexMap.put(s, idx++);
+    	}
     }
 
-    public void setNumStates(int numStates) {
-        this.numStates = numStates;
+    /** 
+     * Given an integer number of states, 
+     * sets the states just starting with character 'S0', 'S1' etc.
+     * To define states that are more meaningful, use the setStates method with array of state strings.
+     * 
+     * @param numStates
+     */
+    public void setStates(int numStates) {
+    	states = new String[numStates];
+    	for (int i = 0;i<numStates;i++) {
+    		states[i] = "S" + i;
+    		stateIndexMap.put(states[i], i);
+    	}
+    }
+
+    /** Gets the states. */
+    public String[] getStates() {
+        return states;
+    }
+
+    /** Gets the number of states. */
+    public int getNumStates() {
+        return states.length;
+    }
+    
+    public Map<String, Integer> getStateIndexMap() {
+    	return stateIndexMap;
     }
 
     public double[][] getTransitionProb() {
         return transitionProb;
+    }
+
+    public double getTransitionProb(int fromState, int toState) {
+        return transitionProb[fromState][toState];
     }
 
     public void setTransitionProb(double[][] transitionProb) {
@@ -28,4 +74,32 @@ public class HMM {
     public void setEmissionProb(double[][] emissionProb) {
         this.emissionProb = emissionProb;
     }
+    
+    public double getEmissionProbability(int state, String emission) {
+    	// Firstly, need to get the index of this emission String
+    	int emissionIndex = emissionIndexMap.get(emission);
+    	return emissionProb[state][emissionIndex];
+    }
+
+	public void setEmissions(String[] emissionsArg) {
+		this.emissions = emissionsArg;
+    	int idx = 0;
+    	for (String e: emissionsArg) {
+    		emissionIndexMap.put(e, idx++);
+    	}
+	}
+
+	/** Gets the emission index map to map from String to integer. */
+	public Map<String, Integer> getEmissionIndexMap() {
+		return emissionIndexMap;
+	}
+	
+	/** Gets the emission for the given index. */
+	public String getEmission(int index) {
+		return emissions[index];
+	}
+
+	public String getState(int state) {
+		return states[state];
+	}
 }
--- src/main/java/edu/gwu/cs/ai/hmm/HMMEvaluator.java
+++ src/main/java/edu/gwu/cs/ai/hmm/HMMEvaluator.java
@@ -1,22 +1,110 @@
 package edu.gwu.cs.ai.hmm;
 
+/**
+ * Just a little test harness to test HMMs.
+ * 
+ * @author Amrinder Arora
+ */
 public class HMMEvaluator {
-    private static final int NUM_EXPERIMENTS = 0;
+	private static final int NUM_EXPERIMENTS = 1;
 
-    public static void main(String[] args) {
-        for (int i = 0; i < NUM_EXPERIMENTS; i++) {
-            runOneExperiment();
-            // Create an HMM object
-            // HMM hmm = new HMM();  // Initialize randomly
-            // Generate some random states (based on HMM transition probabilities)
-            // String[] actualStates = generateStates(hmm);
-            // String[] mleStates Viterbi.solve()
-            // Compare actualStates vs. mleStates
-        }        
-    }
+	public static void main(String[] args) {
+		for (int i = 0; i < NUM_EXPERIMENTS; i++) {
+			runCoinExperiment();
+			runDiceExperiment();
+		}
+	}
 
-    private static void runOneExperiment() {
-        // TODO Auto-generated method stub
+	private static void runCoinExperiment() {
+		// Creates an HMM object
+		HMM hmm = new HMM();
+		// States
+		String[] states = { "F", "B", "VB" };
+		hmm.setStates(states);
+		System.out.println(hmm.getStateIndexMap());
+		
+		// Transition probability
+		double[][] transitionProb = { { 0.6, 0.3, 0.1 }, { 0.3, 0.6, 0.1 }, { 0.333, 0.333, 0.334 } };
+		hmm.setTransitionProb(transitionProb);
+		// Emissions
+		String[] emissions = { "H", "T" };
+		hmm.setEmissions(emissions);
+		// EmissionProb
+		double[][] emissionProb = { { 0.5, 0.5 }, { 0.75, 0.25 }, { 1.0, 0.0 } };
+		hmm.setEmissionProb(emissionProb);
 
-    }
+		// Generates some random states (based on HMM transition probabilities)
+		// Initial state, randomly
+//		int initialStateIndex = (int) (Math.random() * states.length);
+//		String initialState = states[initialStateIndex];
+//		for (int i=1;i<20;i++) {
+//			
+//		}
+
+//		String obs = "HTHTHHHHHHTTH";
+		String[] observations = { "H", "T", "H", "T", "H", "H", "H", "H", "H", "T", "H", "T", "H", "H", "H" };
+		Viterbi v = new Viterbi();
+		double[] initialProb = { 1.0, 0, 0}; //  / 3, 1.0 / 3, 1.0 / 3 };
+		String[] mle = v.solve(hmm, initialProb, observations);
+		System.out.print("MLE: ");
+		for (String s: mle) {
+			System.out.print(s + " ");
+		}
+		System.out.println();
+
+		// String[] actualStates = generateStates(hmm);
+		// String[] mleStates Viterbi.solve()
+		// Compare actualStates vs. mleStates
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void runDiceExperiment() {
+		// Creates an HMM object
+		HMM hmm = new HMM();
+		// States
+		String[] states = { "R", "G", "B" };
+		hmm.setStates(states);
+		System.out.println(hmm.getStateIndexMap());
+		
+		// Transition probability
+		double[][] transitionProb = { { 1.0/3, 1.0/3, 1.0/3 }, { 0.0, 0.5, 0.5 }, { 0.0, 0.0, 1.0 } };
+		hmm.setTransitionProb(transitionProb);
+		// Emissions
+		String[] emissions = { "1", "2", "3", "4" };
+		hmm.setEmissions(emissions);
+		// EmissionProb
+		double[][] emissionProb = { { 0.5, 0.25, 0.25, 0.0}, { 0.25, 0.5, 0.25, 0.0 }, { 0.1666, 0.1666, 0.1667, 0.5 } };
+		hmm.setEmissionProb(emissionProb);
+
+		// Generates some random states (based on HMM transition probabilities)
+		// Initial state, randomly
+//		int initialStateIndex = (int) (Math.random() * states.length);
+//		String initialState = states[initialStateIndex];
+//		for (int i=1;i<20;i++) {
+//			
+//		}
+
+//		String obs = "HTHTHHHHHHTTH";
+		String[] observations = { "1", "2", "3", "1", "1", "2", "1", "2", "3", "1", "1" };
+		Viterbi v = new Viterbi();
+		double[] initialProb = { 1.0, 0, 0}; 
+		String[] mle = v.solve(hmm, initialProb, observations);
+		System.out.print("MLE: ");
+		for (String s: mle) {
+			System.out.print(s + " ");
+		}
+		System.out.println();
+
+		// String[] actualStates = generateStates(hmm);
+		// String[] mleStates Viterbi.solve()
+		// Compare actualStates vs. mleStates
+		// TODO Auto-generated method stub
+
+	}
+
+//    /
+//	private static int getRandomInt(int length) {
+//		return (int) (Math.random() * length);
+//	}
 }
